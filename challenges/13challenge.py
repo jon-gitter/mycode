85b5a7ae9ab840024bf2cbf3707b0b134c94e03d
jon-gitter <jonathan.m.gitter@gmail.com>
2023-01-03 23:12:46 +0000

    join and split

--- challenges/13challenge.py
+++ challenges/13challenge.py
@@ -3,5 +3,5 @@
 name = input("What is your name?")
 dayofweek= input("What is the day of the week?")
 print("Hello, ", name, "!", " Happy ", dayofweek, "!", sep="")
-
+print(f"Hello, {name}! Happy {dayofweek}!")
 
