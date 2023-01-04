42af3b3e98776eabfa58d5d59f1a5e6a5c2b75a3
jon-gitter <jonathan.m.gitter@gmail.com>
2023-01-04 17:57:42 +0000

    about complex lists

---
+++ warmups/wednesdaychallenge.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+number= "2"
+
+name= input("What is your name?\n>")
+
+# This is what you should see when print runs-
+# Hi <name>! Welcome to Day 2 of Python Training!
+
+print("Hi " + name.capitalize() + "! Welcome to Day " + number + " of Python Training!")
