3cf98f8968c3da64cd2b758f57f9dfaecb14ae2a
jon-gitter <jonathan.m.gitter@gmail.com>
2023-01-05 00:04:06 +0000

    completed dictionary questions

---
+++ projects/dictonaryproject.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+question1= {"question": "In fly fishing, what is the thinnest section of line called that's directly tied to the fly?",
+  "leader": False,
+  "tippet": True,
+  "ender": False,
+  "fly line": False
+  }
+
+question2= {"question": "What type of fly is typically used to catch fish on the surface of the water?",
+  "dry fly": True,
+  "nymph": False,
+  "streamer": False,
+  "wet fly": False
+  }
+
+question3= {"question": "In fly fishing, what floats on the water and is used to indicate when a fish bites?",
+  "bobber": False,
+  "floater": False,
+  "bite monitor": False,
+  "strike indicator": True
+  }
+
+
+print(question1)
+print(question2)
+print(question3)
